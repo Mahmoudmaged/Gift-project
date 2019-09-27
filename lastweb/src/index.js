0f15cb4a7e3c68d441839744814ceef532773529
mahmoudelwan <mahmoudelwan460@gmail.com>
2019-09-27 02:18:26 +0200

    Full-Edition

---
+++ lastweb/src/index.js
@@ -0,0 +1,8 @@
+
+require('./src/indexx.js');
+require('./src/help.js');
+require('./src/index1.js');
+require('./src/login.js');
+require('./src/register.js');
+require('./src/task.js');
+require('./src/task2.js');
